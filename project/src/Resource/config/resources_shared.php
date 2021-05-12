74e8c920b01c57845495c01a1fcf5c5d772d9e6e
ilyas <ilyasus@list.ru>
2021-05-12 15:58:16 +0600

    Release v1.5.0

--- project/src/Resource/config/resources_shared.php
+++ project/src/Resource/config/resources_shared.php
@@ -16,6 +16,15 @@ return [
         'migration_dir' => 'src/Resource/propel/migration',
         'migration_table' => 'feed_propel_migration',
     ],
+    'pg' => [
+        'real_host' => 'PG_REAL_HOST',
+        'host' => 'PG_HOST',
+        'port' => 'PG_PORT',
+        'database' => 'PG_DATABASE',
+        'schema' => 'PG_SCHEMA',
+        'user' => 'PG_USER',
+        'password' => 'PG_PASSWORD',
+    ],
     'database' => [
         'db' => 'PG_DATABASE',
         'schema' => 'PG_SCHEMA',
